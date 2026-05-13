fbed08b7e0ecf00750d536e4ebdf727870400977
Saint-Pablo-3 <pawellreshet@icloud.com>
2026-05-13 12:36:47 +0300

    fix: secure kubeconfig for CI/CD

--- infra/terraform/infra-terraform/main/backend.tf
+++ infra/terraform/infra-terraform/main/backend.tf
@@ -9,9 +9,6 @@ terraform {
 
     key = "terraform.tfstate"
 
-    access_key = "REMOVED"
-    secret_key = "REMOVED"
-
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
